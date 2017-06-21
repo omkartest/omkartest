cd3a4769383bed5ae4397c91ed0782fa11e07f48
Omkar.Guhilot <omkar.guhilot@gmail.com>
2017-06-21 17:19:39 +0530

    first project commit

---
+++ omkartest/sample.m
@@ -0,0 +1,18 @@
+//
+//  sample.m
+//  omkartest
+//
+//  Created by Omkar.Guhilot on 21/06/17.
+//  Copyright © 2017 omkarguhilot. All rights reserved.
+//
+
+#import "sample.h"
+
+@implementation sample
+
+-(NSString *) returnInputValue : (NSString *)inputVa
+{
+    return [NSString stringWithFormat:@"your value is %@", inputVa];
+}
+
+@end
